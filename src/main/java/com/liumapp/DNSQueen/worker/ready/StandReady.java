1db099894bad9a17efd6993ddeedb23338a3e604
liumapp <liumapp.com@gmail.com>
2017-07-14 19:28:00 +0800

    add job for woker

---
+++ src/main/java/com/liumapp/DNSQueen/worker/ready/StandReady.java
@@ -0,0 +1,33 @@
+package com.liumapp.DNSQueen.worker.ready;
+
+import java.util.List;
+
+/**
+ * Created by liumapp on 7/14/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public interface StandReady {
+
+    /**
+     * when wife says, what you should do?
+     *
+     * @param whatWifeSays
+     */
+    public String doWhatYouShouldDo(String whatWifeSays);
+
+    /**
+     * Which kind of job you will do after hear what wife says?
+     *
+     * @return
+     */
+    public Class<? extends JobTodo> whatKindOfJobWillYouDo();
+
+    /**
+     * Assign jobs to you!
+     *
+     * @param jobs
+     */
+    public void setJobs(List<? extends JobTodo> jobs);
+
+}
